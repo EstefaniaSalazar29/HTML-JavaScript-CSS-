52c50555cef83b4fb2c960685648f60e6bbd7bb6
Estefania Salazar Valencia <estefaniasalazarv@iesanagustin.edu.co>
2022-04-09 00:20:37 -0500

    Tarea-02

--- script.js
+++ script.js
@@ -6,46 +6,61 @@ let button3 = document.getElementById("button3");
 let button4 = document.getElementById("button4");
 
 
-button1.onclick = function(){
+button1.onclick = function (){
     const valor1 = num1.value;
     const valor2 = num2.value;
-    let result = Math.min(valor1, valor2);
-    alert("El numero menor es " + result);
-    
-}
-
-
-button2.onclick = function(){
-    const valor1 = num1.value;
-    const valor2 = num2.value;
-    let result = Object.is(valor1, valor2);
-    alert(result);
-}
-
-
-
-button3.onclick = function(){
-    let valor1 = num1.value;
-    let valor2 = num2.value;
-    valor1 = valor1 ^ valor2;
-    valor2 = valor1 ^ valor2;
-    valor1 = valor1 ^ valor2;
-    alert("Este es el primer valor: " + valor1)
-    alert("Este es el segundo valor: " + valor2)
-}
-
-
-
-button4.onclick = function(){
-    let valor1 = num1.value;
-    let valor2 = num2.value;
-    let result;
-    while(valor2 != 0){
-        result = valor1 & valor2;
-        valor1 = valor1 ^ valor2;
-        valor2 = result << 1;
+    if(valor1 != "" && valor2 != ""){
+        let result = Math.min(valor1, valor2);
+       alert("El numero menor es " + result);
+    }else{
+       alert("Ingresa numeros")
     }
-    alert("La suma es " + valor1)
+}
+
+
+button2.onclick = function (){
+    const valor1 = num1.value;
+    const valor2 = num2.value;
+    if(valor1 != "" && valor2 != ""){
+        let result = Object.is(valor1, valor2);
+        alert(result);
+    }else{
+        alert("Ingresa numeros")
+    }
+}
+
+
+
+button3.onclick = function (){
+    let valor1 = num1.value;
+    let valor2 = num2.value;
+    if(valor1 != "" && valor2 != ""){
+        valor1 = valor1 ^ valor2;
+        valor2 = valor1 ^ valor2;
+        valor1 = valor1 ^ valor2;
+        alert("Este es el primer valor: " + valor1)
+        alert("Este es el segundo valor: " + valor2)
+    }else{
+        alert("Ingresa numeros")
+    }
+}
+
+
+
+button4.onclick = function (){
+    let valor1 = num1.value;
+    let valor2 = num2.value;
+    if(valor1 != "" && valor2 != ""){
+        let result;
+        while(valor2 != 0){
+            result = valor1 & valor2;
+            valor1 = valor1 ^ valor2;
+            valor2 = result << 1;
+        }
+        alert("La suma es " + valor1)
+    }else{
+        alert("Ingresa numeros")
+    }
 }
 
    
